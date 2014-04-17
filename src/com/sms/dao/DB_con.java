dcd4e94ce2bafa0ce6b9505a2df56d188e2afe52
duolus <dungmanhou@gmail.com>
2014-04-18 00:04:45 +0800

    修改数据库连接

--- src/com/sms/dao/DB_con.java
+++ src/com/sms/dao/DB_con.java
@@ -1,7 +1,12 @@
 package com.sms.dao;
+import java.sql.Connection;
 import java.util.Properties;
 
 public interface DB_con
 {
+	String url = "";
 	
+	public Connection openConnection();
+	public void setURL(String url);
+	public String getURL();
 }
